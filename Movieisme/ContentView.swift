ce32288a4534212a0ea42cf3df851065847d805e
Nouraibn <Noura_alnassar@hotmail.com>
2025-01-20 16:28:49 +0300

    Initial Commit

---
+++ Movieisme/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Movieisme
+//
+//  Created by Noura on 20/07/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Movieisme/MovieismeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieismeApp.swift
+//  Movieisme
+//
+//  Created by Noura on 20/07/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct MovieismeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
